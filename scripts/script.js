900bc9302d96c38eefe87a1fa9f32e0b132c9f2b
hyeriyoon <109633824+hyeriyoon@users.noreply.github.com>
2023-12-13 12:37:07 -0800

    CSS-Home(animation)

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const circle1El = document.querySelector('.home_img_circle1');
+const circle2El = document.querySelector('.home_img_circle2');
+const bubbleEl = document.querySelector('.home_img_bubble');
+
